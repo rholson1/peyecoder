972335d487e3e25b1ec93cfcf33f9ec451800013
rholson1 <rholson1@wisc.edu>
2020-07-06 11:54:50 -0500

    rename to avoid shadowing peyecoder module

--- run-peyecoder.py
+++ run-peyecoder.py
@@ -3,10 +3,8 @@ from PySide2 import QtWidgets
 from peyecoder.gui import MainWindow
 
 if __name__ == "__main__":
-
     app = QtWidgets.QApplication([])
     widget = MainWindow()
     widget.resize(800, 600)
     widget.show()
-
     sys.exit(app.exec_())
