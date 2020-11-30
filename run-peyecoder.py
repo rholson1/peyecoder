41520f72c8114ae98509db14c869cab78538181e
rholson1 <rholson1@wisc.edu>
2020-11-30 14:54:39 -0600

    Work around for bug in Qt on MacOS Big Sur which prevents display of window.

--- run-peyecoder.py
+++ run-peyecoder.py
@@ -1,6 +1,9 @@
 import peyecoder.gui
 import sys
 
+# Temporary workaround for bug with displaying QT windows in MacOS Big Sur.
+import _tkinter
+
 if __name__ == "__main__":
     peyecoder.gui.run(sys.argv)
 
